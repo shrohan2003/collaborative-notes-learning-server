5c43ed9d22a62b9498035178a8b57e46ca3f140d
shrohan2003 <125924303+shrohan2003@users.noreply.github.com>
2026-09-20 04:04:38 +0600

    feat: add login API

--- src/routes/create-account.route.js
+++ src/routes/create-account.route.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const { createAccount } = require('../controllers/create-account.controller.js')
-
-
+const { createAccount, loginAccount } = require('../controllers/create-account.controller')//new add korchi.
 const router = express.Router()
 
 router.post('/account/create', createAccount);
+router.post('/account/login', loginAccount)//  same
 
 module.exports = router;
